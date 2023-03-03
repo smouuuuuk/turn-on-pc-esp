a2f24e89488ed97f1f4aba81aad86f84eb1c4cd8
smouuuuuk <alexander.cabellop@gmail.com>
2023-03-03 19:54:40 +0100

    Added WiFi and WebServer

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,11 +1,42 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
 #include "secrets.h"
+
+ESP8266WebServer server(80);
+
+void handleRoot(){
+  String rootPage;
+  rootPage += "<html> <head> <title>Manage PC</title> </head> <body> <h1>HOLA!!</h1> </body> </html>";
+  server.send(200, "text/html", rootPage);
+  
+}
 
 void setup(){
   Serial.begin(115200);
-  Serial.println(ssid);
-  Serial.println(pwd);
+  Serial.println("");
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SECRET_SSID, SECRET_PWD);
+  
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  // server.onNotFound(handleNotFound);
+  server.begin();
 }
 
 void loop(){
-  
+  server.handleClient();
 }
