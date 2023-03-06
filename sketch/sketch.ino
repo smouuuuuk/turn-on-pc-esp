0ad18e8a5056f158b267042212a8155df29cb359
smouuuuuk <alexander.cabellop@gmail.com>
2023-03-06 20:52:33 +0100

    Added Representation of PC State to WebServer

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -10,9 +10,7 @@ const int powerButtonPin = 13,
           resetButtonPin = 14,
           ledHighPin = 0,
           ledInPin = 5,
-          ledOutPin = 4;
-
-int timeToRead = 0;       
+          ledOutPin = 4;    
 
 int powerPins[] = {powerButtonPin, resetButtonPin};
 
@@ -27,6 +25,8 @@ int prevLightStatus = 0;  // 0=Off, 1=On
 
 int timeToRelease = 0;
 int currentButtonPressed = 0;  // 0=Nothing, 1=Power On/Off/Force, 2=Reboot
+
+const String stateColors[] = {"#f03131", "#5cdf28", "#3743f0"};
 
 void pressPowerButton(int time){
   digitalWrite(powerPins[currentButtonPressed-1], HIGH);
@@ -47,10 +47,6 @@ void checkIfNeedToRelease(){
 
 void checkLEDPowerLight(){
   int stateLED = digitalRead(ledInPin);
-  if (millis() >= timeToRead){
-    Serial.println(stateLED);
-    timeToRead += 500;
-  }
   digitalWrite (ledOutPin, stateLED);
   if (stateLED && !prevLightStatus){
     lastOn = millis();
@@ -64,20 +60,25 @@ void checkLEDPowerLight(){
 void checkStatusChange(int stateLED){
   if(lastOn != 0 && lastOff != 0){
     if((millis() < lastOn + 4000) && (millis() < lastOff + 4000)){
+      if (statePC != 2) Serial.println("State: Sleep");
       statePC = 2;
       return;
     }
   }
   if(stateLED){
+    if (statePC != 1) Serial.println("State: On");
     statePC = 1;
   } else {
+    if (statePC != 0) Serial.println("State: Off");
     statePC = 0;
   }
 }
 
 void handleRoot(){
   String rootPage;
-  rootPage += "<html style=\"font-family:Arial,Helvetica,sans-serif\"><head><title>Manage PC</title></head><body style=\"height:100vh;width:100vw;display:grid;place-items:center;padding:0;margin:0\"><a href=\"onoff\" style=\"font-size:3em;color:#000;text-decoration:none;height:100%;width:100%;border-bottom:2px solid #000;display:grid;place-items:center\" onmouseover='this.style.backgroundColor=\"#000000\",this.style.color=\"white\"' onmouseout='this.style.backgroundColor=\"\",this.style.color=\"black\"'>ON/OFF</a><a href=\"reboot\" style=\"font-size:3em;color:#000;text-decoration:none;height:100%;width:100%;border-bottom:2px solid #000;display:grid;place-items:center\" onmouseover='this.style.backgroundColor=\"#000000\",this.style.color=\"white\"' onmouseout='this.style.backgroundColor=\"\",this.style.color=\"black\"'>RESTART</a><a href=\"force\" style=\"font-size:3em;color:#000;text-decoration:none;height:100%;width:100%;border-bottom:2px solid #000;display:grid;place-items:center\" onmouseover='this.style.backgroundColor=\"#000000\",this.style.color=\"white\"' onmouseout='this.style.backgroundColor=\"\",this.style.color=\"black\"'>FORCE</a></body></html>";
+  rootPage += "<html style=\"font-family:Arial,Helvetica,sans-serif; box-sizing: border-box; display: grid; place-items: center; height: 100vh; padding: 1em; background-color: ";
+  rootPage += stateColors[statePC];
+  rootPage += "\"> <head> <title>Manage PC</title> </head> <body style=\"height:100%;width:100%;display:grid;place-items:center;padding:0;margin:0; border: 1px solid black; background-color: white;\"> <a href=\"onoff\" style=\"font-size:3em;color:#000;text-decoration:none;height:100%;width:100%;border-bottom:2px solid #000;display:grid;place-items:center\" onmouseover='this.style.backgroundColor=\"#000000\",this.style.color=\"white\"' onmouseout='this.style.backgroundColor=\"\",this.style.color=\"black\"'>ON/OFF</a> <a href=\"reboot\" style=\"font-size:3em;color:#000;text-decoration:none;height:100%;width:100%;border-bottom:2px solid #000;display:grid;place-items:center\" onmouseover='this.style.backgroundColor=\"#000000\",this.style.color=\"white\"' onmouseout='this.style.backgroundColor=\"\",this.style.color=\"black\"'>RESTART</a> <a href=\"force\" style=\"font-size:3em;color:#000;text-decoration:none;height:100%;width:100%;border-bottom:2px solid #000;display:grid;place-items:center\" onmouseover='this.style.backgroundColor=\"#000000\",this.style.color=\"white\"' onmouseout='this.style.backgroundColor=\"\",this.style.color=\"black\"'>FORCE</a> </body> </html>";
   server.send(200, "text/html", rootPage);
 }
 
